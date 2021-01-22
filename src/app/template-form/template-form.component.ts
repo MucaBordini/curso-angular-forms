b5fc8ac2f4e211211ed6be79084c582e6f066aa7
MucaBordini <mucabordini@gmail.com>
2021-01-22 11:59:33 -0300

    Pesquisando endereço automaticamente com CEP

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -17,9 +18,22 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.usuario);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  consultaCep(cep){
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != "") {
+      var validaCep = /^[0-9]{8}$/;
+      
+      if (validaCep.test(cep)) {
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+        .subscribe(data => { console.log(data); });
+      }
+    }
   }
 
 }
