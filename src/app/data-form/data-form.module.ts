b9e9f07e8f85b9a382bf9df441947ccbef62fa74
MucaBordini <mucabordini@gmail.com>
2021-01-22 14:39:57 -0300

    Sincronizando HTML com FormGroup

--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    DataFormComponent
+    DataFormComponent,
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule
   ]
 })
 export class DataFormModule { }
